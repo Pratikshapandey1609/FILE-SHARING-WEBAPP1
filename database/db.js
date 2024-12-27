ca76d42a4ef55e93ea4f891998d3db55c80738a6
Pratikshapandey1609 <139357332+Pratikshapandey1609@users.noreply.github.com>
2024-12-27 12:22:28 +0530

    Add files via upload
    commit changes

---
+++ database/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const DBConnection = async ()=>{
+    const MONGO_URI = "mongodb://127.0.0.1:27017/FileSharing"
+    try{
+       await mongoose.connect(MONGO_URI);
+      console.log('database connected sucessfully')
+    } catch(error){
+        console.log('Error while connecting with the database',error.message)
+    }
+}
+
+export default DBConnection;
